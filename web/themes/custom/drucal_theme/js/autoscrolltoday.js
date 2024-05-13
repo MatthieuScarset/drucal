54dc5aab0d5bcdf8207a31b41cbdf34f3ac26748
Matthieu SCARSET <m@matthieuscarset.com>
2024-05-13 12:02:19 +0200

    feat: Improve calendar with jump to date

---
+++ web/themes/custom/drucal_theme/js/autoscrolltoday.js
@@ -0,0 +1,12 @@
+(function () {
+  Drupal.behaviors.autoScrollToday = {
+    attach: function (context) {
+      once('autoscrolltoday', '[data-calendar-view-today]', context).forEach(function (el) {
+        let rect = el.getBoundingClientRect();
+        setTimeout(() => {
+          window.scrollTo({top: Math.floor(rect.top - 10), behavior: "smooth"});
+        }, 500);
+      });
+    }
+  }
+})(Drupal, once);
